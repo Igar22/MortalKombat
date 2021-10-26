6396cf4afa920388d6fe5cc5c672fa64da535d98
Igar22 <84812836+Igar22@users.noreply.github.com>
2021-10-26 22:44:04 +0300

    Add files via upload

--- MortalKombat/main.js
+++ MortalKombat/main.js
@@ -1,3 +1,9 @@
+import { player1, player2 } from "./player.js";
+import { getRandom } from "./utils.js";
+import { enemyAttack } from "./utils.js";
+import { playerAttack } from "./utils.js";
+import createPlayer from "./player.js";
+import { $form } from "./utils.js";
 const $arenas = document.querySelector(".arenas");
 const $randomButton = document.querySelector(".button");
 const $chat = document.querySelector(".chat");
@@ -41,88 +47,6 @@ const logs = {
     draw: 'Ничья - это тоже победа!'
 };
 
-const HIT = {
-    head: 30,
-    body: 25,
-    foot: 20,
-}
-const ATTACK = ['head', 'body', 'foot'];
-
-const player1 = {
-  player: 1,
-  name: "SCORPION",
-  hp: 100,
-  img: "http://reactmarathon-api.herokuapp.com/assets/scorpion.gif",
-  weapon: ["stick", "bit"],
-  attak,
-  elHP,
-  renderHP,
-  changeHP,
-}
-
-const player2 = {
-  player: 2,
-  name: "SUB-ZERO",
-  hp: 100,
-  img: "http://reactmarathon-api.herokuapp.com/assets/subzero.gif",
-  weapon: ["stick", "bit"],
-  attak,
-  elHP,
-  renderHP,
-  changeHP,
-};
-
-function attak() {
-  console.log(this.name + " " + "Fight...")
-}
-
-function createElement (tag, className) {
-  const $tag = document.createElement(tag);
-  if (className) {
-    $tag.classList.add(className);
-  }
- 
-  return $tag;
-}
-
-
-function createPlayer(playerObj) {
-  const $player = createElement("div", "player"+playerObj.player);
-  const $progressbar = createElement("div", "progressbar");
-  const $character = createElement("div", "character");
-  const $life = createElement("div", "life");
-  const $name = createElement("div", "name");
-  const $img = createElement("img");
-
-  $life.style.width = playerObj.hp + "%";
-  $name.innerText = playerObj.name;
-  $img.scr = playerObj.img;
-
-  $player.appendChild($progressbar);
-  $player.appendChild($character);
-  $progressbar.appendChild($life);
-  $progressbar.appendChild($name);
-  $character.appendChild($img);
-
-  return $player;
-}
-
-function elHP() {
-  return document.querySelector(".player"+this.player+" .life");
-}
-
-function changeHP(randomNumber) {
-  this.hp -= randomNumber;
- 
-  if (this.hp <= 0) {
-      this.hp = 0;
-  }
-}
-
-function renderHP() {
-  this.elHP().style.width = this.hp + "%";
-}
-
 function playerWin(name) {
   const $winTitle = createElement("div", "winTitle");
 
@@ -133,10 +57,6 @@ function playerWin(name) {
   }
 
   return $winTitle;
-}
-
-function getRandom(num){
-  return Math.ceil(Math.random() * num);
 }
 
 console.log(player2.elHP())
@@ -156,38 +76,6 @@ function createReloadButton() {
 
 $arenas.appendChild(createPlayer(player1));
 $arenas.appendChild(createPlayer(player2));
-
-function enemyAttack() {
-  const hit = ATTACK[getRandom(3) - 1];
-  const defence = ATTACK[getRandom(3) - 1];
-
-  return {
-    value: getRandom(HIT[hit]),
-    hit,
-    defence,
-  };
-}
-
-function playerAttack() {
-  const attack = {};
-
-  for (let item of $form) {
-    if(item.checked && item.name === "hit") {
-      attack.value = getRandom(HIT[item.value]);
-      attack.hit = item.value;
-    }
-    if(item.checked && item.name === "defence") {
-      attack.defence = item.value;
-      console.log(attack.defence)
-    }
-
-    item.checked = false;
-  }
-
-  return attack;
-}
-
-const $form = document.querySelector(".control");
 
 function showResult() {
 
@@ -227,20 +115,25 @@ function generateLogs(type, player1, player2, value) {
       break;
     case "end":
       el = `<p>${end}</p>`;
+      break;
   }
   $chat.insertAdjacentHTML("afterbegin", el);
 }
 
 function showStartMessage (type) {
   const date = new Date();
-  const start = logs[type].replace("[player1]", player1.name).replace("[player2]", player2.name).replace("[time]", date.getHours() +":"+ date.getMinutes());
+  const start = logs[type].replace("[player1]", player1.name).replace("[player2]", player2.name).replace("[time]", date.getHours() +":"+ date.getMinutes() +":"+ date.getSeconds());
   const el = `<p>${start}</p>`;
   console.log(start);
+
 
   $chat.insertAdjacentHTML("afterbegin", el);
 }
 
 showStartMessage("start");
+
+  // const showTime = () => {const time = new Date().date.getHours() +":"+ date.getMinutes() +":"+ date.getSeconds(); return time};
+  // console.log(showTime)
 
 $form.addEventListener("submit", function(event) {
   event.preventDefault();
